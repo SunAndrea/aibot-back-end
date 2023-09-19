23eda996e8cfa22b31e8cd46004bd615ab189bee
Andrii Shtohryn <andriishtohryn@gmail.com>
2023-09-19 11:41:39 +0300

    add reset link

--- src/controllers/auth/forgotPassword.js
+++ src/controllers/auth/forgotPassword.js
@@ -18,13 +18,13 @@ const forgotPassword = async (req, res) => {
     user.resetPasswordToken = token;
     await user.save();
     const { password, ...userResponse } = user._doc;
-    const resetPasswordLink = `${BASE_URL}/reset-password/${token}`;
+    // const resetPasswordLink = `${BASE_URL}/reset-password/${token}`;
     const message = {
       to: email,
       from: SANDGRID_EMAIL,
       subject: "Скидання пароля",
-      text: `Для скидання пароля перейдіть за цим посиланням: ${resetPasswordLink}`,
-      html: `<p>Для скидання пароля перейдіть за <a href="${resetPasswordLink}">цим посиланням</a></p>`,
+      text: `Для скидання пароля перейдіть за цим посиланням: https://nastyasavchenko.github.io/EVA-I/reset-password`,
+      html: `<p>Для скидання пароля перейдіть за <a href="https://nastyasavchenko.github.io/EVA-I/reset-password">цим посиланням</a></p>`,
     };
 
     await sgMail.send(message);
